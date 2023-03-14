d4ca196f98d65d6340a37593e6c325132775616c
João Pedro Domingues <55238134+jooaopedrod@users.noreply.github.com>
2023-03-14 20:22:59 -0300

    primeira entrega

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main() {
+    //declaração de variaveis
+    int tamanhoSala, qntDose;
+
+    printf("João Pedro Domingues\n");
+    printf("Sistema de informação de processos de vacinação de uma clinica médica\n\n");
+
+    //solicitar nome, tamanho da sala e quantidade de doses disponiveis
+
+    printf("------Seja bem vindo a Clinica X------\n");
+    printf("Por favor, informe o tamanho da sala de triagem: \n");
+    scanf("%d", &tamanhoSala);
+    printf("Informe a quantidade de dose disponiveis: \n");
+    scanf("%d", &qntDose);
 
     return 0;
 }
